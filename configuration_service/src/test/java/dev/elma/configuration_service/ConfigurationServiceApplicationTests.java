0ccc7568152b085ba8ea2419020c575b80cb8a6d
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-12-24 21:43:13 +0100

    commit configs

---
+++ configuration_service/src/test/java/dev/elma/configuration_service/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.configuration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
